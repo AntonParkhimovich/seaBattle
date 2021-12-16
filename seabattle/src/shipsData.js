fae106a3ddc45e2619072a7f3019df9fc29fd751
Anton Parkhimovich <pad1907@gmail.com>
2021-12-16 05:06:45 +0300

    feat: add shipsData

---
+++ seabattle/src/shipsData.js
@@ -0,0 +1,11 @@
+ const shipsData = [{ deck: 1, shipId: 0 },
+    { deck: 1, shipId: 1 },
+    { deck: 1, shipId: 2 },
+    { deck: 1, shipId: 3 },
+    { deck: 2, shipId: 4 },
+    { deck: 2, shipId: 5 },
+    { deck: 2, shipId: 6 },
+    { deck: 3, shipId: 7 },
+    { deck: 3, shipId: 8 },
+    { deck: 4, shipId: 9 }]
+export default shipsData    
