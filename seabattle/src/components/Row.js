e35380dd1f374bb6541fbee232ad15550c351893
Anton Parkhimovich <pad1907@gmail.com>
2021-12-19 07:23:04 +0300

    feat: add cell arragement and cell game

--- seabattle/src/components/Row.js
+++ seabattle/src/components/Row.js
@@ -1,9 +1,17 @@
-import { Cell } from "./Cell"
-export const Row =({row, index})=>{
-  
+import { CellArragement } from "./CellArragement"
+import {CellGame}from './CellGame'
+import { useSelector } from "react-redux"
+export const Row =({row, index, player})=>{
+    const component = useSelector(state=> state.gameState.component)
     return(
         <div className = {'row'} id={index} >
-            {row.map((cell, index)=> <Cell cell={cell} index= {index}/>)}
+            {row.map((cell, index)=> {
+            if(component === 'arragement'){
+                return<CellArragement cell={cell} key={index}index={index}/>
+            }if(component === 'game'){
+                return <CellGame cell={cell} index={index} key={index} player={player}/>
+            }
+            })}
         </div>
     )
 }
