b826aa15023dcbe3943b88aedcd2baa998a621c8
Anton Parkhimovich <pad1907@gmail.com>
2021-12-19 07:24:43 +0300

    refactor: refacror field re used component

--- seabattle/src/components/gameStateComponents/Arragement.js
+++ seabattle/src/components/gameStateComponents/Arragement.js
@@ -1,15 +1,16 @@
-
+import { useSelector } from "react-redux"
 import { Field } from "../Field"
 import NamePlayer from "../NamePlayer"
 import ShipContainer from "./ShipContainer"
 export const Arragement = ()=>{
+    const field = useSelector(state=>state[state.gameState.move].field)
     return(
         <div className ={'arragement'}>
             <div className={'arragement-header'}>
              <NamePlayer/>   
             </div>
             <div className={'arragement-main'}>
-            <Field/>
+            <Field field = {field}/>
             <ShipContainer/>
             </div>
         </div>
