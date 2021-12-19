5bf650cb129a495c04ead23cb63ff88d4415d1b8
Anton Parkhimovich <pad1907@gmail.com>
2021-12-19 07:24:12 +0300

    fix: dropIndex

--- seabattle/src/components/Ship.js
+++ seabattle/src/components/Ship.js
@@ -1,13 +1,12 @@
 import { useDrag } from "react-dnd"
 import { useSelector } from "react-redux";
-export const Ship = ({ship})=>{
+export const Ship = ({ship, index})=>{
     const directionShips = useSelector(state=> state.gameState.directionShips)
     const [{ opacity }, drag, preview] = useDrag(() => ({
         type: 'box',
-        item:{shipId:ship.shipId},
+        item:{index},
         collect: (monitor) => ({
-            opacity: monitor.isDragging() ? 0.4 : 1,
-            transform: monitor.isDragging()? 'rotate(90deg)': ''
+            opacity: monitor.isDragging() ? 0.4 : 0.8,
         }),
     }));
     return (<div ref={preview} style={{opacity}} className={`ship${ship.deck}${directionShips}`}>
