51d7da48f3c9ed2d9a1b3c792199fd30fdd62af0
Anton Parkhimovich <pad1907@gmail.com>
2021-12-19 07:22:33 +0300

    fix: player name coponent

--- seabattle/src/components/NamePlayer.js
+++ seabattle/src/components/NamePlayer.js
@@ -6,13 +6,19 @@ const NamePlayer = () => {
     const move = useSelector(state => state.gameState.move)
     const name = useSelector(state => state[move].name)
     const [playerName, setPlayerName] = useState('')
+    const clickHandler = ()=>{
+        if(playerName){
+            dispatch(changePlayerName(playerName))
+            setPlayerName('')
+        }
+    }
     return (<>
-        <h1 className={'arragement-header__text'}>{name ? `Hello admiral ${name}!` :
+        <h1 className={'title'}>{name ? `Hello admiral ${name}!` :
             'Hello admiral! Please identify yourself'}</h1>
         {name ? null :
             <>
                 <input className={'input'} type={'text'} value={playerName} onChange={({ target }) => setPlayerName(target.value)} />
-                <button className={'button'} onClick={() => dispatch(changePlayerName(playerName))}>Submit</button>
+                <button className={'button'} onClick={clickHandler}>Submit</button>
             </>
         }
 
