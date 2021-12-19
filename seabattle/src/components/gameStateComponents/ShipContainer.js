254435283af8972b62a799a92903fc8b4fe47f0a
Anton Parkhimovich <pad1907@gmail.com>
2021-12-19 07:26:06 +0300

    feat: add random generation

--- seabattle/src/components/gameStateComponents/ShipContainer.js
+++ seabattle/src/components/gameStateComponents/ShipContainer.js
@@ -1,11 +1,15 @@
 import { useDispatch,useSelector } from "react-redux"
-import { initFieldMatrix, returnShips } from "../../store/gameSlice"
+import { initFieldMatrix, returnShips, changeGameState} from "../../store/gameSlice"
 import { changeDirectionShips } from "../../store/gameSlice"
+import { changeMove } from "../../store/gameSlice"
 import { Ship } from "../Ship"
+import { RandomGenerationShips } from "../RandomGenerationShips"
+
 
 const ShipContainer = ()=>{
     const dispatch = useDispatch()
-    const move = useSelector(state=> state.gameState.move)
+    const {move, directionShips} = useSelector(state=> state.gameState)
+    const player =  useSelector(state=> state[move].name)
     const shipsData = useSelector(state => state[move].ships)
     const clickHandler = ()=>{
         dispatch(initFieldMatrix(move))
@@ -13,11 +17,15 @@ const ShipContainer = ()=>{
     }
     return(
         <div className= {'ship-container'}>
-            <div className={'ship-container__main'}>
-                {shipsData.map((ship)=> <Ship ship= {ship}/>)}
+            <div className={'ship-container__main'} style={{flexDirection: directionShips=== 'horizontally'?'column':'row'}}>
+                {shipsData.map((ship, index)=> <Ship ship= {ship} key={index} index={index}/>)}
             </div>
-            <button onClick={()=>dispatch(changeDirectionShips())}> change direction ships</button>
-            <button onClick={clickHandler}>clear battlefield</button>
+            <button className={'button'} onClick={()=>dispatch(changeDirectionShips())}> change direction ships</button>
+            <button className={'button'} onClick={clickHandler}>clear battlefield</button>
+            <RandomGenerationShips/>
+            {shipsData.length === 0&& player ?
+             <button className={'button'} onClick={()=>move==='player1'? dispatch(changeMove()): dispatch(changeGameState('changePlayer'))}>{move === 'player2'?'Start Battle!':'Go next' }</button>
+             :null}
         </div>
     )
 }
